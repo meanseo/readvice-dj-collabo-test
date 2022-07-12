ce6fa7376b633a421a5115eadb9d7d43c3fc23f9
BeautyKim <kima2012114@gmail.com>
2022-07-12 09:57:26 +0900

    first commit

--- admin/settings.py
+++ admin/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'readvicedb',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'NAME': 'readvice',
+        'USER': 'readvice',
+        'PASSWORD': 'readvice',
+        'HOST': 'readvice.cv21xkw7olky.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306'
     }
 }
